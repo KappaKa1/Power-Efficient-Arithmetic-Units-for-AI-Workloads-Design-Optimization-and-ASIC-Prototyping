f5706d0a2f8114d429afee7a165471ecb4ec67e2
KappaKa1 <147872475+KappaKa1@users.noreply.github.com>
2026-04-14 16:46:14 +0200

    Add files via upload

---
+++ rtl/my_file/mux2to1.sv
@@ -0,0 +1,12 @@
+module mux2to1 #(
+  parameter int unsigned BIT_WIDTH   	= 8
+)(
+    input  logic 	 		sel_i,      // select
+    input  logic [BIT_WIDTH-1:0] 	a_i,      // input 0
+    input  logic [BIT_WIDTH-1:0] 	b_i,      // input 1
+    output logic [BIT_WIDTH-1:0] 	y_o       // output
+);
+
+    assign y_o= sel_i ? b_i : a_i;
+
+endmodule
