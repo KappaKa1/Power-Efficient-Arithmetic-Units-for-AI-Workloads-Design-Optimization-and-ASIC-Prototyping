3631c06ef3360412a3181295adb0b26279ebea78
KappaKa1 <147872475+KappaKa1@users.noreply.github.com>
2026-04-22 17:57:05 +0200

    Update shift_reg.sv

--- Verilator/rtl/my_file/shift_reg.sv
+++ Verilator/rtl/my_file/shift_reg.sv
@@ -1,18 +1,22 @@
-module shift_reg (
-  input  logic clk_i,
-  input  logic rst_ni,
-  input  logic bit_i,
-  input  logic enable_i,
-  output logic data_o
+`timescale 1ns/1ps
+
+module shift_registers_in #(
+  parameter int unsigned BIT_WIDTH   = 64,
+  parameter int unsigned NO_OF_PORTS = 16
+)(
+  input  logic                    clk_i,
+  input  logic                    rst_ni,
+  input  logic [NO_OF_PORTS-1:0]  streamed_i,
+  input  logic                    enable_i,
+  output logic [BIT_WIDTH-1:0]    data_o
 );
 
   always_ff @(posedge clk_i) begin
     if (!rst_ni) begin
-      data_o <= 1'b0;
-    end
-    else if (enable_i) begin
-      data_o <= bit_i;
+      data_o <= '0;
+    end else if (enable_i) begin
+      data_o <= {data_o[BIT_WIDTH-NO_OF_PORTS-1:0], streamed_i};
     end
   end
-
+  
 endmodule
