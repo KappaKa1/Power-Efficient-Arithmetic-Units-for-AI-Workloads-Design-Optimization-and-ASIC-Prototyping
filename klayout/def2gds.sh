7268c18b0f10feb993c216a2833e09289d5cdc78
VLSI2_FS26 <vlsi2_26fs26@tardis-b29.ee.ethz.ch>
2026-05-14 10:55:27 +0200

    Added KLayout and Calibre files

---
+++ klayout/def2gds.sh
@@ -0,0 +1,41 @@
+#! /bin/csh -f
+
+# klayout batch mode for transferring def to gds
+#
+# v1 Zerun Tue 27 Feb 13:49:08 CET 2024
+# - initial version
+source ../.setPDK.csh
+
+### modify variables ###
+set topcell = <placeholder>
+set defpath = <placeholder>
+#########################
+
+set outfile = ./out/${topcell}.gds
+
+if ( -e $outfile ) then
+    echo "Removing existing file: $outfile"
+    rm -f $outfile
+endif
+
+oseda -2026.02 klayout -zz -rd design_name=$topcell \
+            -rd in_def=$defpath \
+            -rd in_files=" ../technology/gds/sg13cmos5l_stdcell.gds \
+  ../technology/gds/ez130_8t.gds \
+  ../technology/gds/sg13cmos5l_io.gds \
+  ../technology/gds/bondpad5l_70x70.gds \
+  ../technology/gds/RM_IHPSG13_1P_1024x64_c2_bm_bist.gds \
+  ../technology/gds/RM_IHPSG13_1P_2048x64_c2_bm_bist.gds \
+  ../technology/gds/RM_IHPSG13_1P_256x48_c2_bm_bist.gds \
+  ../technology/gds/RM_IHPSG13_1P_256x64_c2_bm_bist.gds \
+  ../technology/gds/RM_IHPSG13_1P_512x64_c2_bm_bist.gds \
+  ../technology/gds/RM_IHPSG13_1P_64x64_c2_bm_bist.gds \
+" \
+            -rd out_file=./out/${topcell}.gds \
+            -rd tech_file=./${technology}.lyt \
+            -rd layer_map='' \
+            -rd seal_file='' \
+            -rd config_file='' \
+            -rm .klayout/def2stream.py
+
+
---
+++ klayout/run_drc.sh
@@ -0,0 +1,44 @@
+#! /bin/csh -f
+
+# v1.0 Zerun Thu 29 Feb 16:16:23 CET 2024
+# - klayout batch mode running drc
+source ../.setPDK.csh
+### set your gds and topcell ###
+set gds_path = <placeholder>
+set topcell  = <placeholder>
+################################
+
+#usage: 
+#    run_drc.py (--help | -h)
+#    run_drc.py --path=<file_path>
+#            [--table=<table_name>]... [--mp=<num_cores>] [--run_dir=<run_dir_path>]
+#            [--topcell=<topcell_name>] [--run_mode=<mode>] [--drc_json=<json_path>]
+#            [--precheck_drc] [--disable_extra_rules] [--no_feol] [--no_beol] [--no_density]
+#            [--density_thr=<density_threads>] [--density_only] [--antenna]
+#            [--antenna_only] [--no_offgrid] [--no_recommended]
+
+
+### keep the version to 2025.03 for drc running at the moment, because other version run time is 20 times longer
+
+rm ./drc/out/croc_*
+
+time oseda -2026.04 python3 ./drc/tech/run_drc.py \
+ --path=$gds_path \
+ --topcell=$topcell \
+ --run_dir ./drc/out \
+ --no_recommended \
+ --no_density \
+ --antenna \
+ --no_feol \
+ --no_offgrid \
+ --mp 32
+
+# time oseda -2026.04 python3 $IHP_TECH/klayout/tech/drc/run_drc.py \
+#  --path=$gds_path \
+#  --topcell=$topcell \
+#  --run_dir ./drc/out \
+#  --no_recommended \
+#  --no_density \
+#  --antenna \
+#  --mp 32
+
